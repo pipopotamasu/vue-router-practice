8bfbc931c2d21ee56f7af7e472cdfeb4912108c1
pipopotamasu <ec0156hx@gmail.com>
2019-02-07 00:42:02 +0900

    ざっくりAPI実装

---
+++ src/views/Search.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="search">
+    <ul class="items">
+      <li class="item" v-for="(item, index) in items" :key="index">{{ item.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: []
+    }
+  },
+  created () {
+    fetch('https://qiita.com/api/v2/items').then(res => {
+      return res.json()
+    }).then(items => {
+      this.items = items
+    })
+  }
+}
+</script>
